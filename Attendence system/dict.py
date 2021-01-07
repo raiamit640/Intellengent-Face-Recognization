4b5cc933d5a42eb2184c14234bbdd69b9d1e04dd
DANDLA SAIKUMAR <54064000+Saidsp19@users.noreply.github.com>
2021-01-07 22:32:10 +0530

    Add files via upload

---
+++ Attendence system/dict.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 00:08:03 2020
+
+@author: ASM
+"""
+
+
+import numpy as np
+
+name_dict = {0:'Avinash R' ,
+             1:'Durgendra Pandey',
+             2:'Rokkam Hari Sankar',
+             3:'Adurti Sai Mahesh',
+             4:'Manish Pratap Singh',
+             5:'RVNK Neeraj',
+             6:'Saikumar D',
+             7:'Boora Shiva',
+             8:'Jated Vikas',
+             9:'Vinod G',
+             }
+
+np.save('name_dict.npy',name_dict) 
